c3ffb4b9f2dcc47c2c52624b73df8d70b1107531
yassir346 <yassir.mahir98@gmail.com>
2025-10-07 16:06:19 +0100

    Création de hibernate et JPA pour la persistence des donéées et ORM.

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+    }
+}
---
+++ src/main/java/Song.java
@@ -0,0 +1,35 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "song")
+
+// POJO class
+public class Song {
+
+    @Id @Column(name = "songId") private int id;
+
+    @Column(name = "songName") private String songName;
+
+    @Column(name = "singer") private String artist;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getSongName() { return songName; }
+
+    public void setSongName(String songName)
+    {
+        this.songName = songName;
+    }
+
+    public String getArtist() { return artist; }
+
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
+}
